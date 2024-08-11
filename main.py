f447db239462de0544fdc6960afcd9e19e21cab9
Anirudh Chidambaram <anirudh.chida@gmail.com>
2024-08-12 01:15:22 +1000

    Implemented Walking Animation by juggling sprites

--- main.py
+++ main.py
@@ -17,9 +17,44 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+
+def load_sprite_sheets(
+    folder1: str, folder2: str, width: int, height: int, direction=False
+):
+
+    path = join("assets", folder1, folder2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    # print(f"{images=}")
+    all_sprites = {}
+
+    for image in images:
+        # print(f"{image=}")
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+        # print(all_sprites)
+    return all_sprites
+
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "VirtualGuy", 32, 32, True)
+    ANIMATION_DELAY = 4
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -47,13 +82,28 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+
+        self.animation_count += 1
 
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        # pygame.draw.rect(window, self.COLOR, self.rect)
+        # self.sprite = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_bg(name):
@@ -72,7 +122,6 @@ def get_bg(name):
 def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
-
     player.draw(window)
 
     pygame.display.update()
@@ -90,7 +139,7 @@ def handle_move(player):
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_img = get_bg("Blue.png")
+    background, bg_img = get_bg("Brown.png")
     run = True
 
     player = Player(100, 100, 50, 50)
